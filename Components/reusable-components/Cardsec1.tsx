973377150815fd0d15e765df942bb8e549508c15
Muhammad Hanif Zuhair <hzuhair192@gmail.com>
2024-07-01 13:13:51 +0700

    Update  Section Card Swiper 1

--- Components/reusable-components/Cardsec1.tsx
+++ Components/reusable-components/Cardsec1.tsx
@@ -1,17 +1,26 @@
+"use client"
+
 import Image from 'next/image';
-import Swiper from 'swiper';
+import {Swiper,SwiperSlide} from 'swiper/react';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import {Pagination} from 'swiper/modules'
+import '@/app/globals.css'
+
 
 function Card(imageUrl :string, title :string, desc :string){
     return(
-        <div className='flex grow-1 flex-col items-center text-center'>
-            <Image src={imageUrl} width={140} height={100} alt='' />
-            <h3 className='font-bold text-2xl mt-4 mb-2'>
-                {title}
-            </h3>
-            <p className='text-md'>
-                {desc}
-            </p>
-        </div>
+        <SwiperSlide className='flex items-center mb-10'>
+            <div className='flex flex-col items-center text-center'>
+                <Image src={imageUrl} width={140} height={100} alt='' />
+                <h3 className='font-bold text-xl mt-4 mb-2'>
+                    {title}
+                </h3>
+                <p className='text-sm md:text-md'>
+                    {desc}
+                </p>
+            </div>
+        </SwiperSlide>
     )
 }
 
@@ -19,22 +28,33 @@ export default function Cardsec1(){
     return (
         <div className="flex h-40 bg-gray-900 w-full relative h-[450px] items-end">
             <div className="h-3/4 bg-white w-full rounded-t-6xl relative">
-                <div className='flex absolute -top-[75px] mx-auto'>
-                    {Card (
-                        '/gojek-1.svg',`Fortune's favorite`,
-                        `Southeast Asia's only company to make it to Fortune's 'Change the World' list twice.`)}
-                    {Card (
-                        '/gojek-2.svg',`$7.1 b contribution`,
-                        `Value added to the Indonesian economy in 2019.`
-                    )}
-                    {Card (
-                        '/gojek-3.svg',`Tonne times greener`,
-                        `Saved 11.3 tonnes of plastic in cutlery since October 2020.`
-                    )}
-                    {Card (
-                        '/gojek-4.svg',`Digisting merchant`,
-                        `Resulted in 80% growth in number of GoFood merchants during pandemic.`
-                    )}
+                <div className='absolute -top-[75px] w-full'>
+                    <Swiper className='md:max-w-[80%] max-w-[80%] mx-auto'
+                        breakpoints={{
+                            500:{
+                                slidesPerView:2
+                            },
+                            800:{
+                                slidesPerView: 4
+                        }
+                        }}
+                        spaceBetween={30} loop={true} pagination={true} modules={[Pagination]}>
+                            {Card (
+                                '/gojek-1.svg',`Fortune's favorite`,
+                                `Southeast Asia's only company to make it to Fortune's 'Change the World' list twice.`)}
+                            {Card (
+                                '/gojek-2.svg',`$7.1 b contribution`,
+                                `Value added to the Indonesian economy in 2019.`
+                            )}
+                            {Card (
+                                '/gojek-3.svg',`Tonne times greener`,
+                                `Saved 11.3 tonnes of plastic in cutlery since October 2020.`
+                            )}
+                            {Card (
+                                '/gojek-4.svg',`Digisting merchant`,
+                                `Resulted in 80% growth in number of GoFood merchants during pandemic.`
+                            )}
+                    </Swiper>
                 </div>
             </div>
         </div>
