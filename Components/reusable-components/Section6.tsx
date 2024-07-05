83d7e2005eadbe2b093915deaf7dece29ae0d452
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2024-07-05 20:55:50 +0700

    Add Section6

---
+++ Components/reusable-components/Section6.tsx
@@ -0,0 +1,15 @@
+const Section6=()=>{
+    return(
+        <div className="bg-gray-950 py-12 w-full">
+            <div className="text-white flex flex-col items-center gap-2">
+                <h1 className="text-center w-4/5 text-2xl font-bold">We build products that nations run on</h1>
+                <button className="rounded-3xl bg-[#00880d] w-fit py-3 px-5 text-white font-bold">
+                    View all products
+                </button>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Section6
