91c222d6b5602e839744deb7fca74a374b5d6ee3
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2024-07-08 12:01:08 +0700

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Cardsec1 from "../Components/reusable-components/Cardsec1";
 import Cardsec2 from "../Components/reusable-components/Cardsec2";
 import Cardsec3 from "../Components/reusable-components/Cardsec3";
 import Section4 from "../Components/reusable-components/Section4";
+import Section5 from "../Components/reusable-components/Section5";
 import Section6 from "../Components/reusable-components/Section6";
 import Section7 from "../Components/reusable-components/Section7";
 import Footer from "../Components/reusable-components/Footer";
@@ -17,6 +18,7 @@ export default function Home() {
       <Cardsec2 />
       <Cardsec3 />
       <Section4 />
+      <Section5 />
       <Section6 />
       <Section7 />
       <Footer />
