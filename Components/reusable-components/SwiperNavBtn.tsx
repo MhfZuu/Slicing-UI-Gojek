761be57584b129501b2cd6d1c1d3cc3cfe240f4e
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2024-07-10 11:29:23 +0700

    Update SwiperNavBtn

--- Components/reusable-components/SwiperNavBtn.tsx
+++ Components/reusable-components/SwiperNavBtn.tsx
@@ -5,11 +5,11 @@ export default function NextPrevBtn(){
     const swiper = useSwiper();
     return(
         <div className="absolute flex justify-between w-full top-1/3 md:top-1/4 z-30">
-            <button className='flex justify-center w-10 h-10 rounded-full bg-gray-300 items-center' 
+            <button className='flex justify-center w-12 h-12 rounded-full bg-gray-300 items-center opacity-75' 
                     onClick={()=>swiper.slidePrev(300,false)}>
                         <BiChevronLeft className="h-6 w-6"/>
             </button>
-            <button className='flex justify-center w-10 h-10 rounded-full bg-gray-300 items-center' 
+            <button className='flex justify-center w-12 h-12 rounded-full bg-gray-300 items-center opacity-75' 
                     onClick={()=>swiper.slideNext(300,false)}>
                         <BiChevronRight className="h-6 w-6"/>
             </button>
