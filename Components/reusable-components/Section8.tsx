b8e6da42cb5b6eb21f407fe60cc4c831fedff8ae
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2024-07-10 11:29:51 +0700

    Update Section 8

--- Components/reusable-components/Section8.tsx
+++ Components/reusable-components/Section8.tsx
@@ -3,10 +3,11 @@
 import Image from 'next/image'
 import { Pagination } from 'swiper/modules'
 import {Swiper,SwiperSlide} from 'swiper/react'
-import SwiperNavBtn from "../reusable-components/SwiperNavBtn"
+import SwiperNWorldBtn from '../reusable-components/SwiperNWorldBtn'
 import "swiper/css"
 import "swiper/css/pagination"
 import '@/app/globals.css'
+import { useState } from 'react';
 
 const SlideCard= (contents:any[]) => {
     return (
@@ -15,7 +16,7 @@ const SlideCard= (contents:any[]) => {
                 <SwiperSlide className='h-auto mb-10 flex justify-center'>
                     <div className="text-black text-center flex flex-col items-center space-y-4">
                         <h1 className="font-extrabold text-2xl lg:text-4xl">{content.country}</h1>
-                        <p className="text-sm font-normal lg:text-md pt-4 w-[85%] lg:4/5">{content.desc}</p>
+                        <p className="text-sm font-normal lg:text-md pt-4 w-4/5">{content.desc}</p>
                         <div className='bg-[#00880d] rounded-3xl px-6 py-3 w-fit'>
                             <p className='font-semibold text-white text-sm'>
                                 Enter newsroom
@@ -29,23 +30,31 @@ const SlideCard= (contents:any[]) => {
 }
 
 const Section8 = () => {
+    const [rotation, setRotation] = useState(0)
+    const [activeIdx, setActiveIdx] = useState(0)
+
     return (
-        <section className=" bg-[#4ac9e3] h-screen py-12 overflow-hidden">
+        <section className=" bg-[#4ac9e3] lg:h-screen h-[55vh] py-12 overflow-hidden">
             <div className="lg:container mx-auto h-full">
-                <Swiper className='lg:w-2/3 justify-center'
+                <Swiper className='lg:w-2/3 md:w-4/5 w-[95vw]justify-center'
                     slidesPerView={1}
                     pagination={true}
+                    onRealIndexChange={(swiper)=>{
+                            setActiveIdx(swiper.realIndex)
+                        }
+                    }
                     modules={[Pagination]}>
 
                     {SlideCard(contents)}
                     <div className='absolute top-1/4 w-full'>
-                        <SwiperNavBtn />
+                        <SwiperNWorldBtn setRotation={setRotation} activeIdx={activeIdx} />
                     </div>
                 </Swiper>
                 <div>
                     <Image src='https://lelogama.go-jek.com/prime/upload/image/General_impact_-_Globe.svg'
-                            width={100} height={100} alt='gojek world' 
-                            className='w-full aspect-[2/1]lg:aspect-[3/2]'/>
+                            width={100} height={100} alt='gojek world'
+                            className='transition ease-linear duration-300 w-full aspect-[2/1]lg:aspect-[3/2]'
+                            style={{transform: `rotate(${rotation}deg)`}}/>
                 </div>
             </div>
         </section>
@@ -62,7 +71,7 @@ const contents =[
     },{
         "country":"Singapore",
         "desc"   :"Clocking 30 million trips in its first year of operation - a threadfold increase in just six months - Gojek is scalling like a ragging bull"   
-    },,{
+    },{
         "country":"Vietnam",
         "desc"   :"Over 80,000 GoFood merchants and 150,000 driver partners help million of our customer eat, move, repeat!"   
     }
