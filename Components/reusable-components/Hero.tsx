9886a268f6728b61cd4b857469b47ebe49137507
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2025-06-30 17:14:42 +0700

    Update link

--- Components/reusable-components/Hero.tsx
+++ Components/reusable-components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     <div className="pt-[80px] lg:pt-[96px] z-0">
       <div className="w-full lg:aspect-video  aspect-[3/4] flex flex-col h-auto items-center">
         <iframe
-          src="https://www.youtube.com/embed/067TW2O8OfQ?enablejsapi=1&version=3&controls=0&rel=0&mute=1&playlist=/067TW2O8OfQ&playsinline=1&autoplay=1&loop=1"
+          src="https://www.youtube.com/embed/067TW2O8OfQ?enablejsapi=1&version=3&controls=0&rel=0&mute=1&playlist=067TW2O8OfQ&playsinline=1&autoplay=1&loop=1"
           className="pointer-none hidden lg:block absolute z-10 w-full aspect-video"
         />
         <Image
